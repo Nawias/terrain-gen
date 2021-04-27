31915bfa53132349d4f14f0535ba3e0bb83210f2
Michał Wójcik <bronybracket@gmail.com>
2021-04-27 13:22:57 +0200

    Dodaj pliki projektów.

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,91 @@
+﻿/**
+    Michał Wójcik 2021
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MouseLook : MonoBehaviour
+{
+    //Mnożnik czułości myszy
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+    //Mnożnik prędkości poruszania się
+    [SerializeField]
+    private float movementSpeed = 10f;
+    //Zmienna pomocnicza do śledzenia obrotu w osi X (patrzenie w górę i w dół)
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        //Zablokowanie kursora do swobodnego poruszania kamerą
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        UpdateRotation();
+        UpdatePosition();
+    }
+
+    private void UpdateRotation()
+    {
+        //Pobranie pozycji myszy w osiach X i Y
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Aktualizacja rotacji w osi X
+        xRotation -= mouseY;
+        //Ograniczenie rotacji do 180 stopni
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Rotacja w osi X (góra-dół)
+        transform.localRotation = Quaternion.Euler(xRotation, transform.localEulerAngles.y, 0f);
+        
+        //Rotacja w osi Y (na boki)
+        transform.Rotate(Vector3.up, mouseX);
+    }
+    private void UpdatePosition()
+    {
+        //Składowe wektora ruchu
+        float x = 0f;
+        float z = 0f;
+        float y = 0f;
+
+        //Pobieranie wartości z klawiatury
+        //Ruch w danej osi = kierunek (-1,1) * prędkość ruchu * czas od ostatniej klatki
+        if (Input.GetKey(KeyCode.W))
+        {
+            z += movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            z -= movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x -= movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x += movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            y += movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            y -= movementSpeed * Time.deltaTime;
+        }
+
+        //Utworzenie wektora ruchu ze składowych
+        Vector3 move = transform.right * x + transform.forward * z + transform.up * y;
+        //Wykonanie ruchu przy pomocy komponentu CharacterController - relatywnie do aktualnej rotacji
+        GetComponent<CharacterController>().Move(move);
+    }
+
+}
---
+++ Assets/TerrainRenderer.cs
@@ -0,0 +1,119 @@
+﻿/**
+    Michał Wójcik 2021
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainRenderer : MonoBehaviour
+{
+    //Obiekt służący za szablon dla obiektów renderujących linie
+    [SerializeField]
+    private LineRenderer lineRenderer;
+    //Wczytana tekstura mapy wysokości
+    [SerializeField]
+    private Texture2D heightMap;
+    //Wartość mnożnika wysokości terenu
+    [SerializeField]
+    [Range(1, 10f)]
+    private float terrainHeight;
+    //Property definiujące dostęp do zmiennej terrainHeight
+    public float TerrainHeight
+    {
+        get
+        {
+            return terrainHeight;
+        }
+        set
+        {
+            terrainHeight = value;
+            SetVertices();
+        }
+    }
+    //Bufory na obiekty renderujące linie
+    private LineRenderer[] vertLineRenderers;
+    private LineRenderer[] horLineRenderers;
+
+    void Start()
+    {
+        if(heightMap == null)
+        {
+            return;
+        }
+
+        // Pionowych linii jest tyle ile poziomo pikseli, a poziomych linii tyle ile pionowo pikseli.
+        vertLineRenderers = new LineRenderer[heightMap.width];
+        horLineRenderers = new LineRenderer[heightMap.height];
+        
+        // Tworzenie obiektów renderujących linie pionowe
+        for (int i = 0; i < heightMap.width; i++)
+        {
+            // Z szablonu tworzony jest nowy LineRenderer. Każdy ma tyle punktów, ile pionowo jest pikseli.
+            LineRenderer lr = Instantiate<LineRenderer>(lineRenderer, transform);
+            lr.positionCount = heightMap.height;
+            // W pętli ustawiana jest pozycja każdego wierzchołka linii - szerokość i długość to współrzędne pikseli,
+            // a wysokość to wartość koloru w skali szarości pomnożona przez mnożnik wysokości terenu.
+            for (int j = 0; j < heightMap.height; j++)
+            {
+                lr.SetPosition(j, new Vector3(i, heightMap.GetPixel(i, j).grayscale*terrainHeight, j));   
+            }
+            // Obiekt renderujący zachowywany jest w buforze dla ułatwienia dostępu do późniejszej modyfikacji wierzchołków
+            vertLineRenderers[i] = lr;
+        }
+
+        //Tworzenie obiektów renderujących linie poziome - analogicznie do linii pionowych, z zamianą osi X oraz Z.
+        for (int i = 0; i < heightMap.height; i++)
+        {
+            LineRenderer lr = Instantiate<LineRenderer>(lineRenderer, transform);
+            lr.positionCount = heightMap.width;
+            for (int j = 0; j < heightMap.width; j++)
+            {
+                lr.SetPosition(j, new Vector3(j, heightMap.GetPixel(j, i).grayscale*terrainHeight, i));
+            }
+            horLineRenderers[i] = lr;
+        }
+
+        // Wyłączenie rysowania na scenie obiektu będącego szablonem.
+        lineRenderer.gameObject.SetActive(false);
+    }
+
+    // Metoda służąca do aktualizacji wysokości wierzchołków po pierwszym renderze
+    private void SetVertices()
+    {
+        // W pętli ustawiana jest pozycja każdego wierzchołka poszczególnych linii - szerokość i długość to współrzędne pikseli,
+        // a wysokość to wartość koloru w skali szarości pomnożona przez mnożnik wysokości terenu.
+        
+        // Dla linii pionowych
+        for (int i = 0; i < heightMap.width; i++)
+        {
+            for (int j = 0; j < heightMap.height; j++)
+            {
+                vertLineRenderers[i].SetPosition(j, new Vector3(i, heightMap.GetPixel(i, j).grayscale * terrainHeight, j));
+            }
+        }
+
+        // Dla linii dla linii poziomych
+        for (int i = 0; i < heightMap.height; i++)
+        {
+            for (int j = 0; j < heightMap.width; j++)
+            {
+                horLineRenderers[i].SetPosition(j, new Vector3(j, heightMap.GetPixel(j, i).grayscale * terrainHeight, i));
+            }
+        }
+    }
+
+    private void Update()
+    {
+        // Klawisz  [  zmniejsza mnożnik wysokości terenu, a klawisz  ]  zwiększa go.
+        // Użycie property TerrainHeight aktualizuje wszystkie wierzchołki po zmianie wartości.
+        if (Input.GetKey(KeyCode.LeftBracket))
+        {
+            TerrainHeight -= 1f * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightBracket))
+        {
+            TerrainHeight += 1f * Time.deltaTime;
+        }
+    }
+}
